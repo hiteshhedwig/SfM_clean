117a631cb4e1e6cc5e8b93bc6eb5e0916d5edeb3
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-09-16 21:33:34 +0530

    remove redundant variable

--- pose.py
+++ pose.py
@@ -14,21 +14,18 @@ def decompose_essential_matrix(essential_matrix):
     # Singular Value Decomposition (SVD)
     U, S, Vt = svd(essential_matrix)
 
-    # define W, Z matrix 
+    # define W 
     # The Z matrix is a skew-symmetric matrix. A skew-symmetric matrix is a matrix that is the negative of its transpose.
+    # The product UWVT gives one of the possible rotation matrices. 
+    # This is because W represents a rotation of 90 degrees about the z-axis. 
+    # When you multiply U and VT with W in between, 
+    # you're effectively combining the rotations from the two camera views with this 90-degree rotation. 
+    # This results in one of the possible rotation matrices that align the two camera views based on the epipolar geometry.
     W = np.array([
             [0, -1, 0],
             [1,  0, 0],
             [0,  0, 1]
     ])
-
-    Z = np.array(
-        [
-            [0, 1, 0],
-            [-1,  0, 0],
-            [0,  0, 0]
-        ]
-    )
 
     # two possible rotation matrices
     Rot_1 = U@W@Vt

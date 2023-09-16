49eb27830a42a4a820bc0cfbf3bd8d3448ebd618
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-09-16 20:38:29 +0530

    init commit

---
+++ feature.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+# 1. **Feature Extraction**: Identify points of interest in each image. This can be done using feature detection algorithms such as SIFT, SURF, or ORB.
+# 2. **Feature Matching**: Find the same points of interest in multiple images. If two images have a set of points in common, it's possible they are observing the same part of a scene. 
+
+def feature_extraction_and_matching(img0, img1):
+    # Create a SIFT object
+    sift = cv2.SIFT_create()
+    # Detect and compute SIFT features for both images
+    kp1, des1 = sift.detectAndCompute(img0, None)
+    kp2, des2 = sift.detectAndCompute(img1, None)
+
+    # Perform brute-force matching with ratio test
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    # For each pair of matches (m, n) obtained:
+    # m is the best match and n is the second-best match.
+    good_matches = []
+    for m, n in matches:
+        # The ratio test checks the quality of the matches.
+        # If the distance of the best match (m.distance) is significantly smaller than that of the second-best match (n.distance),
+        # then the match is considered to be "good".
+        # In this case, if m's distance is less than 75% of n's distance, it's added to the good_matches list.
+        if m.distance < 0.75 * n.distance:
+            good_matches.append(m)
+
+    # Extract the coordinates of matched keypoints
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches])
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches])
+
+    # Use RANSAC to identify inliers
+    # The fundamental matrix encapsulates the epipolar geometry between two views and is a fundamental concept in stereo vision and structure from motion.
+    fundamental_mat, inliers = cv2.findFundamentalMat(src_pts, dst_pts, cv2.FM_RANSAC)
+    if inliers is None:
+        exit(0)
+
+    # Filter matches using the inliers
+    ransac_matches = [good_matches[i] for i, val in enumerate(inliers) if val == 1]
+    return fundamental_mat, kp1, kp2, ransac_matches
+
+def demo_matched_image(img0, img1, kp1, kp2, ransac_matches):
+    img_matches = cv2.drawMatches(img0, kp1, img1, kp2, ransac_matches, None)
+    img_matches = cv2.resize(img_matches, (1920,1080))
+    cv2.imshow("Matches " , img_matches)
+    cv2.waitKey(0)
---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2
+from matplotlib import pyplot as plt
+
+### Steps in SfM:
+# 1. **Feature Extraction**: Identify points of interest in each image. This can be done using feature detection algorithms such as SIFT, SURF, or ORB.
+# 2. **Feature Matching**: Find the same points of interest in multiple images. If two images have a set of points in common, it's possible they are observing the same part of a scene. 
+from feature import feature_extraction_and_matching,demo_matched_image
+# 3. **Estimate Motion**: Using the matched points, estimate the camera's motion between the two images. This is typically done using techniques like the eight-point algorithm for fundamental matrix estimation, which can be followed by a triangulation method to determine 3D points in space.
+
+# 4. **Bundle Adjustment**: Refine the estimated camera poses and 3D point positions simultaneously. This step minimizes the reprojection error, which is the difference between the observed position of a point in an image and the projected position of the estimated 3D point using the camera pose.
+
+# 5. **Dense Reconstruction**: After determining camera poses and sparse 3D points from the previous steps, you can proceed to reconstruct the scene densely. This involves understanding which parts of the images correspond to which 3D points (i.e., establishing a dense correspondence between multiple images).
+
+# 6. **Meshing and Texturing**: Once you have a dense cloud of 3D points, you can create a mesh or surface from them. This is often followed by "draping" the images over this surface to produce a textured 3D model.
+
+
+INTRINSIC_MATRIX = [
+    [1733.74,  0,     792.27],
+    [0,      1733.74, 541.89],
+    [0,        0,     1]
+]
+
+def load_image(filename):
+    return cv2.imread(filename)
+
+def main():
+    DEBUG = True
+
+    img_0_path = "data/artroom1/im0.png"
+    img_1_path = "data/artroom1/im1.png"
+
+    img0 = load_image(img_0_path)
+    img1 = load_image(img_1_path)
+
+    fundamental_mat, kp1, kp2, ransac_matches = feature_extraction_and_matching(img0, img1)
+
+    if DEBUG:
+        demo_matched_image(img0, img1, kp1, kp2, ransac_matches)
+
+
+
+if __name__ == '__main__':
+    main()
